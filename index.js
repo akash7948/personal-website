14baefc24dc578a6f905155a9c062f1035a7b7ab
akash7947 <akashchauhan_dalbir@srmuniv.edu.in>
2020-05-17 10:33:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var options = {
+  strings: ['<i>html</i>', '<i>css</i>', '<i>javascript</i>', '<i>react</i>', '<i>redux</i>'],
+  typeSpeed: 40,
+  loop: true
+};
+
+var typed = new Typed('.highlight', options);
